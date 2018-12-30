fead990eae125cebe0db57360b06da1e18092305
MauricioHz <mauriciohz2002@gmail.com>
2018-12-30 17:59:23 -0300

    Antes de hacer funcionalidades para recuperar cuenta.

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $array = array('saludo' => 'hola');
+        return response()->json($array, 201);
+    }
+}
